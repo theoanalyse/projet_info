a39f97bf3b6bb2da533926dddb50285bd700fae2
Théo <theoandre1907@gmail.com>
2020-11-18 11:48:30 +0100

    hey

--- algorithm_A_star.py
+++ algorithm_A_star.py
@@ -1,7 +1,26 @@
 import graph as gr
 
-def A*(Graph, start, finish)
+def heuristic(vertex1, vextex2) :
+    return 1
+
+def extract_minimum(F) :
+    return
+
+def A_star(Graph, start, finish) :
+    g_score = {}
+    f_score = {}
+    closed_list = []
     F = [start]
-    p = []
+    p = {}
     g_score[start] = 0
-    f_score[start] = g_score + heuristic(start, finish)
+    f_score[start] = g_score[start] + heuristic(start, finish)
+
+    while F != [] :
+        u = extract_minimum(F)
+        if u == finish :
+            return (p, g_score[finish])
+        closed_list+= [u]
+
+        for v in Graph.outgoing_neighbors() :
+            if not(v in closed_list) :
+                g_score_try = g_score[u] + g
--- shortest_path.py
+++ shortest_path.py
@@ -1,6 +1,22 @@
 import math as m
 import parser as par
+import graph as g
+import dijkstra as d
 
-coordinates = par.parse_csv("input/coordinates.csv")
+ports = par.parse_csv("input/coordinates.csv")
 adjacency_matrix = par.parse_adjacency_matrix("input/adjacency_matrix.txt")
 
+graph_ports = g.Graph()
+for port in ports:
+    x, y, port_name = port.get_coordinates_radians()
+    graph_ports.add_vertex( (port_name, x, y) )
+
+# print(graph_ports.list_vertices())
+
+available_ports = 0
+for i in range(len(adjacency_matrix)):
+    for j in range(len(adjacency_matrix[i])):
+        if adjacency_matrix[i][j] != -1:
+            available_ports += 1
+print(available_ports)
+print(graph_ports.is_connex())
